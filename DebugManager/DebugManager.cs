ce8015ac03b0442d0881ccb7a123641efc8d294a
Daniel Dang <daniel.dang@outlook.com>
2020-04-16 18:56:40 +1000

    added debugmanager

---
+++ DebugManager/DebugManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugManager : Singleton<DebugManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+
+        Debug.unityLogger.logEnabled = Debug.isDebugBuild;
+    }
+
+}
